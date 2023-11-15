afa01c1aab389aab732373770d6e686674bf855d
Jscott7227 <150862732+Jscott7227@users.noreply.github.com>
2023-11-14 18:01:16 -0600

    Add files via upload

--- Pubg.R
+++ Pubg.R
@@ -1,4 +1,5 @@
 library(readr)
+library(e1071)
 train_V2 <- read_csv("train_V2.csv")
 pubg_test <- read_csv("test_V2.csv")
 
@@ -14,6 +15,16 @@ plot(train_V2$kills ~ train_V2$winPlacePerc)
 plot(train_V2$killPlace ~ train_V2pubg$winPlacePerc)
 
 train_V2 <- na.omit(train_V2)
+train_V2 <- subset(train_V2,select = -c(killPoints, rankPoints, winPoints)) 
+
+
+model <- svm(winPlacePerc ~ kills, train_V2)
+
+predictedY <- predict(model, train_V2)
+
+#points(data$X, predictedY, col = "red", pch=4)
+
+
 solo <- subset(train_V2 , train_V2$matchType == "solo", select = -c(killPoints, rankPoints, winPoints)) 
 
 duo <- subset(train_V2 , train_V2$matchType == "duo", select = -c(killPoints, rankPoints, winPoints)) 
@@ -22,4 +33,6 @@ squad <- subset(train_V2 , train_V2$matchType == "squad", select = -c(killPoints
 
 solo_fpp <- subset(train_V2 , train_V2$matchType == "solo-fpp", select = -c(killPoints, rankPoints, winPoints)) 
 
-duo_fpp <- subset(train_V2 , train_V2$matchType == "duo-fpp", select = -c(killPoints, rankPoints, winPoints)) 
+duo_fpp <- subset(train_V2 , train_V2$matchType == "duo-fpp", select = -c(killPoints, rankPoints, winPoints))
+
+
