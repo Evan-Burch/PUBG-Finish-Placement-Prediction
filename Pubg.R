455aa8c3e797ad053b0c72e527924c4d6f559a9e
Jhoop18 <94129297+Jhoop18@users.noreply.github.com>
2023-11-16 12:40:44 -0600

    Pubg.R

--- Pubg.R
+++ Pubg.R
@@ -1,5 +1,5 @@
-library(readr)
-library(e1071)
+library("readr")
+library("e1071")
 library("corrplot")
 
 train_V2 <- read_csv("train_V2.csv")
@@ -13,32 +13,32 @@ hist(train_V2$walkDistance)
 summary(train_V2)
 summary(pubg_test)
 
-plot(train_V2$kills ~ train_V2$winPlacePerc)
-plot(train_V2$killPlace ~ train_V2pubg$winPlacePerc)
+#plot(train_V2$kills ~ train_V2$winPlacePerc)
+#plot(train_V2$killPlace ~ train_V2$winPlacePerc)
 
-train_V2 <- na.omit(train_V2)
-train_V2 <- subset(train_V2,select = -c(killPoints, rankPoints, winPoints)) 
+train_V2_clean <- na.omit(train_V2)
+train_V2_clean <- subset(train_V2,select = -c(killPoints, rankPoints, winPoints)) 
 
-set <- subset(train_V2, select = -c(Id, groupId, matchId, matchType)) 
+set <- subset(train_V2_clean, select = -c(Id, groupId, matchId, matchType)) 
 corr <- cor(set, method="pearson")
 corrplot(corr, method='color')
 
 #SVR Model
-model <- svm(winPlacePerc ~ kills, train_V2)
+#model <- svm(winPlacePerc ~ kills, train_V2)
 
-predictedY <- predict(model, train_V2)
+#predictedY <- predict(model, train_V2)
 
 #points(data$X, predictedY, col = "red", pch=4)
 
 
-solo <- subset(train_V2 , train_V2$matchType == "solo", select = -c(killPoints, rankPoints, winPoints)) 
+solo <- subset(train_V2_clean , train_V2$matchType == "solo") 
 
-duo <- subset(train_V2 , train_V2$matchType == "duo", select = -c(killPoints, rankPoints, winPoints)) 
+duo <- subset(train_V2_clean , train_V2$matchType == "duo") 
 
-squad <- subset(train_V2 , train_V2$matchType == "squad", select = -c(killPoints, rankPoints, winPoints)) 
+squad <- subset(train_V2_clean , train_V2$matchType == "squad") 
 
-solo_fpp <- subset(train_V2 , train_V2$matchType == "solo-fpp", select = -c(killPoints, rankPoints, winPoints)) 
+solo_fpp <- subset(train_V2_clean , train_V2$matchType == "solo-fpp") 
 
-duo_fpp <- subset(train_V2 , train_V2$matchType == "duo-fpp", select = -c(killPoints, rankPoints, winPoints))
+duo_fpp <- subset(train_V2_clean , train_V2$matchType == "duo-fpp")
 
 
