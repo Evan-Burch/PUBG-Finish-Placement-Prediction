6b2635cb33ac1769a24663a5d734b1236fef5aae
Evan-Burch <127160516+Evan-Burch@users.noreply.github.com>
2023-11-20 18:08:05 -0600

    Update Pubg.R

--- Pubg.R
+++ Pubg.R
@@ -6,6 +6,7 @@ library("corrplot")
 train_V2 <- read_csv("train_V2.csv")
 pubg_test <- read_csv("test_V2.csv")
 
+# Histograms to analyze and explore the dataset
 hist(train_V2$damageDealt)
 hist(train_V2$kills)
 hist(train_V2$matchDuration)
@@ -13,9 +14,6 @@ hist(train_V2$walkDistance)
 
 summary(train_V2)
 summary(pubg_test)
-
-#plot(train_V2$kills ~ train_V2$winPlacePerc)
-#plot(train_V2$killPlace ~ train_V2$winPlacePerc)
 
 # Clean NAs, unused attributes, and outliers from the data
 train_V2_clean <- na.omit(train_V2)
@@ -25,7 +23,7 @@ train_V2_clean <- subset(train_V2_clean, train_V2_clean$kills < 50)
 
 summary(train_V2_clean)
 
-# Subset the data using the 6 match types
+# Method 1 - Subset the data using the 6 match types
 solo <- subset(train_V2 , train_V2$matchType == "solo") 
 
 duo <- subset(train_V2 , train_V2$matchType == "duo") 
@@ -38,18 +36,20 @@ duo_fpp <- subset(train_V2 , train_V2$matchType == "duo-fpp")
 
 squad_fpp <-  subset(train_V2 , train_V2$matchType == "duo-fpp")
 
-# This is for correlation matrix
-
+# Method 2 - Correlation matrix
 set <- subset(train_V2_clean, select = -c(Id, groupId, matchId, matchType)) 
 corr <- cor(set, method="pearson")
 corrplot(corr, method='color')
 
-#SVR Model
+# Method 3 - Linear/Logistic Regression
+#plot(train_V2$kills ~ train_V2$winPlacePerc)
+#plot(train_V2$killPlace ~ train_V2$winPlacePerc)
+
+# Method 4 - Support Vector Regression(SVR)
 #model <- svm(winPlacePerc ~ kills, train_V2)
-
 #predictedY <- predict(model, train_V2)
-
 #points(data$X, predictedY, col = "red", pch=4)
 
+# Method 5 - Decision Tree Regressor
 
 
